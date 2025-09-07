c9db217795345edbb6fb21646d12492ce852f132
franklinnot <franklinnot@outlook.com>
2025-09-07 04:06:54 -0500

    Carga de envs, conexion a la bd, logs de arranque, aplicar dtos, swagger (soporte incompleto)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // Cargar variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Conexión a la bd
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+        dbName: configService.get<string>('DB_NAME'),
+      }),
+    }),
+
+    // Modulos
+    UsuariosModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger(); // Logger para el proceso de arranque
+
+  // Acceder a las variables de entorno
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') || 3000;
+
+  // CONFIGURACIONES GLOBALES
+
+  // Aplicar reglas de DTOS.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // configuracion de Swagger
+  const config = new DocumentBuilder().setTitle('API de Hakuna').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // dispobible en /api/docs
+
+  // asegurrar que la app no se apague abruptamente
+  app.enableShutdownHooks();
+
+  await app.listen(PORT);
+  logger.log(`Aplicación corriendo en http://localhost:${PORT}/api`);
+  logger.log(`Documentación disponible en: http://localhost:${PORT}/api/docs`);
 }
 bootstrap();
